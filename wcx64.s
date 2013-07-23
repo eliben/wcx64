e0ea4edd5c34587b83a61dbf643df00e6bd8bfb8
Eli Bendersky <eliben@gmail.com>
2013-07-23 06:43:59 -0700

    Basic signs of life

--- wcx64.s
+++ wcx64.s
@@ -13,12 +13,79 @@ newline_str:
 buf_for_read:
     # leave space for terminating 0
     .space READBUFLEN + 1, 0x0
+    # We need buf_for_itoa to be large enough to contain a 64-bit integer.
+    # endbuf_for_itoa will point to the end of buf_for_itoa and is useful
+    # for passing to itoa.
+    .set BUFLEN, 32
+buf_for_itoa:
+    .space BUFLEN, 0x0
+    .set endbuf_for_itoa, buf_for_itoa + BUFLEN - 1
 
 #---------------- CODE ----------------#
     .globl _start
     .text
 _start:
+    mov (%rsp), %r12                # argc
+    cmp $1, %r12
+    jle .L_no_argv
 
+    # Call open(argv[1], O_RDONLY).
+    mov 16(%rsp), %rdi
+    mov $O_RDONLY, %rsi
+    mov $OPEN_NO_MODE, %rdx
+    mov $2, %rax
+    syscall
+
+    mov %rax, %rdi
+    call count_in_file
+
+    mov %rax, %r13
+    mov %rdx, %r14
+    mov %r9, %r15
+
+    mov %r13, %rdi
+    lea endbuf_for_itoa, %rsi
+    call itoa
+    mov %rax, %rdi
+    call print_cstring
+    lea newline_str, %rdi
+    call print_cstring
+
+    mov %r14, %rdi
+    lea endbuf_for_itoa, %rsi
+    call itoa
+    mov %rax, %rdi
+    call print_cstring
+    lea newline_str, %rdi
+    call print_cstring
+
+    mov %r15, %rdi
+    lea endbuf_for_itoa, %rsi
+    call itoa
+    mov %rax, %rdi
+    call print_cstring
+    lea newline_str, %rdi
+    call print_cstring
+
+    jmp .L_wcx64_exit
+
+.L_no_argv:
+    mov $0, %rdi
+    call count_in_file
+
+    mov %rax, %r13
+    mov %rdx, %r14
+    mov %r9, %r15
+
+    mov %r13, %rdi
+    lea endbuf_for_itoa, %rsi
+    call itoa
+    mov %rax, %rdi
+    call print_cstring
+    lea newline_str, %rdi
+    call print_cstring
+
+.L_wcx64_exit:
     # exit(0)
     mov $0, %rdi
     mov $60, %rax
@@ -33,13 +100,18 @@ _start:
 #   rdx     word count
 #   r9      char count
 count_in_file:
+    # Save callee-saved registers.
+    push %r12
+    push %r13
+    push %r14
+    push %r15
     # Register usage within the function:
     #
     # rdi: holds the fd
     # dl: next byte read from the buffer
     # r9: char counter
-    # r10: word counter
-    # r11: line counter
+    # r15: word counter
+    # r14: line counter
     # r13: address of the read buffer
     # rcx: loop index for going over a read buffer
     # r12: state indicator, with the states defined below.
@@ -51,8 +123,8 @@ count_in_file:
     # After each call to read(), rax is used for its return value.
 
     xor %r9, %r9
-    xor %r10, %r10
-    mov $1, %r11
+    xor %r15, %r15
+    mov $0, %r14
     lea buf_for_read, %r13
     mov $IN_WHITESPACE, %r12
 
@@ -83,11 +155,11 @@ count_in_file:
     # else, it's not whitespace but a part of a word
     cmp $IN_WORD, %r12
     je .L_done_with_this_byte
-    inc %r10
+    inc %r15
     mov $IN_WORD, %r12
     jmp .L_done_with_this_byte
 .L_seen_newline:
-    inc %r11
+    inc %r14
 .L_seen_whitespace_not_newline:
     cmp $IN_WORD, %r12
     je .L_end_current_word
@@ -107,8 +179,14 @@ count_in_file:
 
     # Done with this file. The char count is already in r9.
     # Put the word and line counts in their return locations.
-    mov %r10, %rdx
-    mov %r11, %rax
+    mov %r15, %rdx
+    mov %r14, %rax
+
+    # Restore callee-saved registers.
+    pop %r15
+    pop %r14
+    pop %r13
+    pop %r12
     ret
 
 # Function print_cstring
@@ -137,4 +215,56 @@ print_cstring:
     mov $1, %rdi
     mov %r10, %rdx
     syscall
+    ret
+
+# Function itoa
+#   Convert an integer to a null-terminated string in memory.
+#   Assumes that there is enough space allocated in the target
+#   buffer for the representation of the integer. Since the number itself
+#   is accepted in the register, its value is bounded.
+# Arguments:
+#   rdi:    the integer
+#   rsi:    address of the *last* byte in the target buffer
+# Returns:
+#   rax:    address of the first byte in the target string that
+#           contains valid information.
+itoa:
+    movb $0, (%rsi)        # Write the terminating null and advance.
+    dec %rsi
+
+    # If the input number is negative, we mark it by placing 1 into r9
+    # and negate it. In the end we check if r9 is 1 and add a '-' in front.
+    mov $0, %r9
+    cmp $0, %rdi
+    jge .L_input_positive
+    neg %rdi
+    mov $1, %r9
+.L_input_positive:
+
+    mov %rdi, %rax          # Place the number into rax for the division.
+    mov $10, %r8            # The base is in r8
+
+.L_next_digit:
+    # Prepare rdx:rax for division by clearing rdx. rax remains from the
+    # previous div. rax will be rax / 10, rdx will be the next digit to
+    # write out.
+    xor %rdx, %rdx
+    div %r8
+    # Write the digit to the buffer, in ascii
+    dec %rsi
+    add $0x30, %dl
+    movb %dl, (%rsi)
+
+    cmp $0, %rax            # We're done when the quotient is 0.
+    jne .L_next_digit
+
+    # If we marked in r9 that the input is negative, it's time to add that
+    # '-' in front of the output.
+    cmp $1, %r9
+    jne .L_itoa_done
+    dec %rsi
+    movb $0x2d, (%rsi)
+
+.L_itoa_done:
+    mov %rsi, %rax          # rsi points to the first byte now; return it.
     ret
